1d5d5cd3c10b843f2279152ac875efb30d14756c
zachary123212 <zlindenbaum@gmail.com>
2017-06-06 13:22:55 -0400

    renamed Room functions to be more clear

--- main.hs
+++ main.hs
@@ -3,6 +3,7 @@ import Text.Printf
 import qualified Data.List as L
 
 -- misc
+
 type ID = Int
 
 keys :: M.Map a b -> [a]
@@ -15,32 +16,38 @@ l !? i | abs i >= length l = Nothing
 -- game structure
 
 data Room = Room
-  { name        :: String
-  , description :: String
-  , paths       :: M.Map String ID
+  { roomName        :: String
+  , roomDesc :: String
+  , roomPaths       :: M.Map String ID
   } deriving (Show, Eq, Ord)
+
+-- data Item = Item
+--   { roomName :: String
+--   , roomDesc  :: String
+--   , canTake :: Bool
+--   } deriving (Show, Eq, Ord)
 
 data GameState = GameState
   { currentRoom :: Room
   } deriving Show
 
--- TODO: make this eventually parse JSON
+-- TODO: make this eventually parse JSON, and eventually use parsec to create custom syntax
 rooms = [
   Room "room 1" "the first room" (M.fromList [("north", 1), ("south", 2)]),
   Room "room 2" "the second room" (M.fromList [("south", 0)]),
   Room "room 3" "the third room" (M.fromList [("north", 0)])]
 
 printCurrentRoom :: GameState -> IO ()
-printCurrentRoom state = let room   = currentRoom state
-                             rPaths = ((L.intercalate ", ") . keys . paths) room
+printCurrentRoom state = let room     = currentRoom state
+                             pathText = ((L.intercalate ", ") . keys . roomPaths) room
                          in putStr $ printf "Room: %s\nDescription: %s\nPaths: %s\n"
-                            (name room) (description room) rPaths
+                            (roomName room) (roomDesc room) pathText
 
 getRoom :: ID -> Maybe Room
 getRoom id = rooms!?id
 
 move :: GameState -> String -> GameState
-move state pathName = let testIndex = ((M.lookup pathName) . paths . currentRoom) state
+move state pathName = let testIndex = ((M.lookup pathName) . roomPaths . currentRoom) state
                       in case testIndex >>= getRoom of
                            Just room -> GameState room
                            Nothing -> state
