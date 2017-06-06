629c292c750c7a891e07b51306b94416ba90857f
zachary123212 <zlindenbaum@gmail.com>
2017-06-06 16:27:02 -0400

    added skeleton of GameState

--- main.hs
+++ main.hs
@@ -22,7 +22,7 @@ data Room = Room
   , roomDesc  :: String
   , roomPaths :: M.Map String ID
   , roomItems :: [Item]
-  } deriving (Show, Eq, Ord )
+  } deriving (Show, Eq, Ord)
 
 data Item = Item
   { itemNames   :: [String]
@@ -35,7 +35,41 @@ data Player = Player
   , currentItems :: [Item]
   } deriving Show
 
+data GameState = GameState
+  { gamePlayer :: Player
+  , gameRooms  :: [Room]
+  } deriving Show
+
 -- TODO: actually make GameState structure
+
+printCurrentRoom :: GameState -> IO ()
+printCurrentRoom state = let room     = (currentRoom . gamePlayer) state
+                             pathText = ((L.intercalate ", ") . keys . roomPaths) room
+                         in putStr $ printf "Room: %s\nDescription: %s\nPaths: %s\n"
+                            (roomName room) (roomDesc room) pathText
+
+getRoom :: ID -> Maybe Room
+getRoom id = rooms!?id
+
+
+-- TODO: resolve inconsistency of player inputs; should everything or nothing take GameState as input?
+move :: Player -> String -> Player
+move player pathName = let testIndex = ((M.lookup pathName) . roomPaths . currentRoom) player
+                      in case testIndex >>= getRoom of
+                           Just room -> Player room (currentItems player)
+                           Nothing -> player
+
+gameLoop :: GameState -> IO ()
+gameLoop state = do
+  System.Process.system "clear"
+  printCurrentRoom state
+  putStrLn "\nEnter command:"
+  command <- getLine
+  case command of
+    "quit" -> exitSuccess
+    _      -> gameLoop $ GameState (move (gamePlayer state) command) (gameRooms state)
+
+--TEST
 
 -- TODO: make this eventually parse JSON, and eventually use parsec to create custom syntax
 rooms = [
@@ -43,35 +77,7 @@ rooms = [
   Room "room 2" "the second room" (M.fromList [("south", 0)]) [],
   Room "room 3" "the third room" (M.fromList [("north", 0)]) []]
 
-printCurrentRoom :: Player -> IO ()
-printCurrentRoom player = let room     = currentRoom player
-                              pathText = ((L.intercalate ", ") . keys . roomPaths) room
-                          in putStr $ printf "Room: %s\nDescription: %s\nPaths: %s\n"
-                             (roomName room) (roomDesc room) pathText
-
-getRoom :: ID -> Maybe Room
-getRoom id = rooms!?id
-
-move :: Player -> String -> Player
-move player pathName = let testIndex = ((M.lookup pathName) . roomPaths . currentRoom) player
-                      in case testIndex >>= getRoom of
-                           Just room -> Player room (currentItems player)
-                           Nothing -> player
-
-gameLoop :: Player -> IO ()
-gameLoop player = do
-  System.Process.system "clear"
-  printCurrentRoom player
-  putStrLn "\nEnter command: "
-  command <- getLine
-  case command of
-    "quit" -> exitSuccess
-    _ -> gameLoop (move player command)
-
-
---TEST
-
 main :: IO ()
 main = do
-  let p = Player (rooms!!0) []
-  gameLoop p
+  let game = GameState (Player (rooms!!0) []) rooms
+  gameLoop game
